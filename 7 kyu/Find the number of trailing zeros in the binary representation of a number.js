e8ccdf8c90b2fcd06662a9081741f1e62f9c0c95
bulat-01 <bulat.dzeytov@internet.ru>
2024-09-18 15:23:30 +0300

    add 7 kyu task solution

---
+++ 7 kyu/Find the number of trailing zeros in the binary representation of a number.js
@@ -0,0 +1,13 @@
+function trailingZeros(n) {
+    let numBin = n.toString(2);
+    let count = 0;
+    for(let i = numBin.length - 1; i >= 0; i--) {
+      if(numBin[i] == '0') {
+          count++
+      } else {
+            break;
+      }
+    }
+    
+    return count;
+  }
