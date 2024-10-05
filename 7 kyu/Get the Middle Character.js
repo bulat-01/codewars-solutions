da4c8cf5cce4374be1cc0c2595c71c78cd88cc86
bulat-01 <bulat.dzeytov@internet.ru>
2024-10-05 14:49:03 +0300

    Update Get the Middle Character.js

--- 7 kyu/Get the Middle Character.js
+++ 7 kyu/Get the Middle Character.js
@@ -1,5 +1,8 @@
-function getMiddle(s)
-{
+/*
+  You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+*/
+function getMiddle(s) {
   let lengthWord = s.length;
+
   return (lengthWord % 2 == 0) ? s[lengthWord / 2 - 1] + s[lengthWord / 2] : s[(lengthWord - 1) / 2 ];
 }
