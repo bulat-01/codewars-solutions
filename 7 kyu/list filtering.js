1d0c3b9edf73cd49bca8af6bdc1eae64551b6dd5
bulat-01 <bulat.dzeytov@internet.ru>
2024-10-05 14:41:42 +0300

    Update list filtering.js

--- 7 kyu/list filtering.js
+++ 7 kyu/list filtering.js
@@ -1,10 +1,14 @@
+/* 
+  In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+*/
+
 function filter_list(l) {
-    for(let i=0; i < l.length; i++) {
-     if(typeof l[i] !== 'number') {
-       l.splice(i,1);
-       i--;
-     }
-   }
-   
-   return l;
- }
+  for (let i = 0; i < l.length; i++) {
+      if (typeof l[i] !== 'number') {
+          l.splice(i, 1);
+          i--;
+      }
+  }
+  
+  return l;
+}
