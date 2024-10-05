9546666cf04d43e8e930f1e19ba9a96244f1ff37
bulat-01 <bulat.dzeytov@internet.ru>
2024-10-05 13:17:56 +0300

    add 6 kyu task solution

---
+++ 6 kuy/Find the odd int.js
@@ -0,0 +1,22 @@
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times. 
+*/
+
+function findOdd(A) {
+    let map = new Map();
+    
+    A.forEach(num => {
+      if(!map.has(num)) {
+        map.set(num, 1)
+      }
+      
+      else {
+        let count = map.get(num) + 1;
+        map.set(num, count)
+      }
+    });
+    
+    return [...map].find(([key, value]) => value  % 2 != 0)[0];
+}
