f9f9e9ed7c72a869274e83d59c2e0e6390012309
bulat-01 <bulat.dzeytov@internet.ru>
2024-10-05 14:47:06 +0300

    Update highest and lowest.js

--- 7 kyu/highest and lowest.js
+++ 7 kyu/highest and lowest.js
@@ -1,6 +1,18 @@
+/*
+    In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+    Notes
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
 function highAndLow(numbers){
     const arrNumbers = numbers.split(' ').map(number => +number);
+
     const max = Math.max(...arrNumbers);
+
     const min = Math.min(...arrNumbers);
+
     return max + ' ' + min;
 }
