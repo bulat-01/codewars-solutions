c973d52d46a88b3f6f8f8ebf998293edff4fdf8d
bulat-01 <bulat.dzeytov@internet.ru>
2024-11-16 16:41:47 +0300

    add 6 kyu task solution

---
+++ js/5 kyu/Directions Reduction.js
@@ -0,0 +1,7 @@
+/*
+    Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+    Example:
+        input: ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+        output: [West] 
+*/
